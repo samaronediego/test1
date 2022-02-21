cc40f413525a7e58c98738988697b2aac2aab62f
samaronediego <100052980+samaronediego@users.noreply.github.com>
2022-02-21 14:22:41 +0000

    Add files via upload

---
+++ vetores_Ex1.java
@@ -0,0 +1,31 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class vetores_Ex1 {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		double[] vect = new double[n]; //criando a variavel vetor 'vect'
+		
+		for (int i = 0; i < n; i++){
+			vect[i] = sc.nextDouble();
+		}
+		double sum = 0.0;
+		for (int i = 0; i < n; i++){
+			sum += vect[i];
+		}
+		
+		double avg = sum/n;
+		
+		System.out.println("AVERAGE HEIGHT: " + avg );
+		
+		sc.close();
+	}
+
+}
